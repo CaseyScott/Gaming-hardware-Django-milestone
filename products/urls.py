fcc08a675dbf50e9571747e2cd6311691ea76544
CSsnakeeater <34281480+CSsnakeeater@users.noreply.github.com>
2019-10-21 16:11:34 +1300

    products page cards and styling

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, ItemDetailView
 
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^(?P<pk>\d+)/$', ItemDetailView.as_view(), name='item_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Product
 
 
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+class ItemDetailView(DetailView):
+    model = Product
