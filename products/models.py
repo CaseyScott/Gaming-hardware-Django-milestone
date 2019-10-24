8a1c84f79c61903057e2bef264f61570951d4983
CSsnakeeater <34281480+CSsnakeeater@users.noreply.github.com>
2019-10-24 16:47:23 +1300

    read more function working ready for sty;ing

--- products/models.py
+++ products/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('V', 'video cards'),
+    ('H', 'hard drives'),
+    ('M', 'memory'),
+    ('C', 'cpu'),
+    ('CL', 'cooling'),
+    ('MB', 'mother boards'),
+    ('P', 'power supplies')
+)
 
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
 
     def __str__(self):
         return self.name
